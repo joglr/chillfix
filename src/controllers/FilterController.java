0e8b344b354878c3ebf55eb55d27cfef1d372560
cupcakestudios <tili@itu.dk>
2019-12-04 17:15:45 +0100

    start on filters.

--- src/controllers/FilterController.java
+++ src/controllers/FilterController.java
@@ -1,4 +1,17 @@
 package controllers;
 
 public class FilterController {
+
+    private String currentFiltres;
+    private String searchFiltres;
+    private String genreFiltres;
+
+    public FilterController(String currentFiltres) {
+
+        this.currentFiltres = currentFiltres;
+        this.genreFiltres = genreFiltres;
+        this.searchFiltres = searchFiltres;
+
+    }
+
 }
