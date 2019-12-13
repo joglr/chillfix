f95f798875e9ef245b127c1af5ceb15ec16e0baf
cupcakestudios <tili@itu.dk>
2019-12-13 15:04:20 +0100

    add comments on myListView class

--- src/views/MyListView.java
+++ src/views/MyListView.java
@@ -1,6 +1,44 @@
-package views;
-
-public class MyListView {
-
-//   Parent root = new root;
-}
+//package views;
+//
+//import javafx.fxml.FXML;
+//import models.Media;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MyListView {
+//
+////   Parent root = new root;
+//}
+//public class Min_Liste_Controller {
+//    private List<Media> my_list;
+//
+//
+//    public Min_Liste_Controller() {
+//        my_list = new ArrayList<>();
+//    }
+//
+//    @FXML
+//// lav togglebutton for save og delete i master
+//    public void saveButton(Media media) {
+////        ActionEvent saveButton;
+//        my_list.add(media);
+//
+//    }
+//
+//    @FXML
+//    //    TODO remove media from list event handler
+////    Action listener/event handler, når knap: "Fjern fra min liste" trykkes:
+//    public void deleteMedia(Media media) {
+//
+//        for (int m = 0; my_list.size() >= m; m++) {
+//            my_list.remove(m);
+//        }
+//    }
+//    @FXML
+//    public void showMyList() {
+////    new root to view my list
+//
+//        displayMedia();
+//    }
+//}
