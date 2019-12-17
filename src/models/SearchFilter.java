6113f0920d34cbcfe1aae40644cf2bf8c229bb33
Simone-haukjaer <58275871+Simone-haukjaer@users.noreply.github.com>
2019-12-17 13:45:24 +0100

    kommentar til SearchFilter

--- src/models/SearchFilter.java
+++ src/models/SearchFilter.java
@@ -4,11 +4,14 @@ public class SearchFilter implements Filter {
     private final String searchString;
 
     public SearchFilter(String searchString) {
+        //med metoden trim() trimmer man whitespaces
         this.searchString = searchString.trim().toLowerCase();
     }
 
     @Override
     public boolean matches(Media m) {
+        //returnerer true hvis m i title, description eller year indeholder searchstring.
+        //differentierer ikke mellem store og små bogstaver
         return m.getTitle().toLowerCase().contains((searchString)) ||
                 m.getDescription().toLowerCase().contains((searchString)) ||
                 Integer.toString(m.getYear()).toLowerCase().contains((searchString));
