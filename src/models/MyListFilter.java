178f334287ace1b7abaefe91c334541a088a41cd
Simone-haukjaer <58275871+Simone-haukjaer@users.noreply.github.com>
2019-12-17 13:37:37 +0100

    VBox, HBox, titel, genre, årstal, osv...

--- src/models/MyListFilter.java
+++ src/models/MyListFilter.java
@@ -6,7 +6,11 @@ public class MyListFilter implements Filter {
     @Override
     public boolean matches(Media m) {
         boolean foundMatch = false;
+
+        //et foreach loop af typen string der hedder 'entry', der kører igenem alle elementer i my_list som tilgåes
+        //igennem metoden getMy_list() osv..
         for (String entry : Min_Liste_Controller.getMy_list()) {
+            //hvis m matcher med et ImbdID, så sættes foundMatch til true, og foreach loopet stoppes.
             if (entry.equals(m.getImdbID())) {
                 foundMatch = true;
                 break;
