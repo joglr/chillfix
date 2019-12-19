cc9de2d5dea723f40eba3eb8da856aeca11b5a91
Jonas Røssum <jglr@itu.dk>
2019-12-19 13:36:01 +0100

    add onAction handler to episode buttons fix seasons

--- src/main/java/views/MediaDetailsView.java
+++ src/main/java/views/MediaDetailsView.java
@@ -3,9 +3,14 @@ package views;
 import controllers.MyListController;
 import controllers.RootController;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import models.Media;
 import models.MyListFilter;
@@ -19,15 +24,28 @@ class MediaDetailsView {
     public MediaDetailsView(Media media) {
         BorderPane root = new BorderPane();
         root.getStyleClass().add("dark");
+        root.getStyleClass().add("ScrollPane");
 
+        ScrollPane wrapper = new ScrollPane();
         BorderPane container = new BorderPane();
         container.getStyleClass().add("bordered");
-        root.setCenter(container);
-        VBox left = new VBox();
-        VBox right = new VBox();
-        HBox leftBottomButtons = new HBox();
 
-        //Knap 'Tilbage' og eventhandler til knappen, igennem en lambda expression
+        wrapper.setContent(container);
+        root.setCenter(wrapper);
+
+        ScrollPane leftWrapper = new ScrollPane();
+        leftWrapper.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        leftWrapper.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        leftWrapper.getStyleClass().add("ScrollPane");
+
+        VBox left = new VBox();
+        leftWrapper.setContent(left);
+        VBox right = new VBox();
+        HBox bottomButtons = new HBox();
+        container.setBottom(bottomButtons);
+
+        // Knap 'Tilbage' og eventhandler til knappen, igennem en lambda expression
         Button backButton = new Button("Tilbage");
         backButton.addEventHandler(ActionEvent.ACTION, (ActionEvent e) -> {
             try {
@@ -36,17 +54,13 @@ class MediaDetailsView {
                 ex.printStackTrace();
             }
         });
-        //Tooltip.install(container, new Tooltip(media.getTitle()));
-        Image poster = null;
-        //container.setPrefHeight();
-        container.setPrefWidth(600);
 
         MediaCard mediaCard = new MediaCard(media, false);
 
-        //Hvis media findes på 'MyList', så dannes en knap 'Fjern fra min liste' med en eventhandler, der fjerner mediet fra listen
+        // Hvis media findes på 'MyList', så dannes en knap 'Fjern fra min liste' med en eventhandler, der fjerner mediet fra listen
         if (new MyListFilter().matches(media)) {
             Button DeleteFromMyListButton = new Button("Fjern fra min liste");
-            leftBottomButtons.getChildren().addAll(DeleteFromMyListButton);
+            bottomButtons.getChildren().addAll(DeleteFromMyListButton);
             DeleteFromMyListButton.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
                 MyListController.getMyList().remove(media.getImdbID());
 
@@ -55,19 +69,16 @@ class MediaDetailsView {
             });
 
         } else {
-            //Hvis media ikke er en del af MyList, så er der mulighed for at tilføje til listen
+            // Hvis media ikke er en del af MyList, så er der mulighed for at tilføje til listen
             Button AddToMyListButton = new Button("Tilføj til min liste");
             AddToMyListButton.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
                 MyListController.getMyList().add(media.getImdbID());
                 new MediaDetailsView(media);
 
             });
-            leftBottomButtons.getChildren().addAll(AddToMyListButton);
+            bottomButtons.getChildren().addAll(AddToMyListButton);
 
         }
-        Region expandRegion = new Region();
-
-        VBox.setVgrow(expandRegion, Priority.ALWAYS);
 
         Text title = new Text(media.getTitle().toUpperCase());
         title.setStyle("-fx-font-size: 25px");
@@ -84,8 +95,8 @@ class MediaDetailsView {
         genre.getStyleClass().add("darkText");
 
         container.setTop(backButton);
-        container.setLeft(left);
-        container.setMaxWidth(600);
+        container.setLeft(leftWrapper);
+        container.setMaxWidth(800);
         container.setMaxHeight(400);
         container.setRight(right);
 
@@ -95,24 +106,37 @@ class MediaDetailsView {
 
         if (media.getType() == Media.Type.SERIES) {
             Series series = (Series) media;
+            int seasonCounter = 1;
             for (int i : series.getSeasons()) {
-                FlowPane season = new FlowPane();
-                HBox hbox = new HBox();
-                for (int ii = 0; ii < i; ii++) {
+                HBox season = new HBox();
+                FlowPane episodes = new FlowPane();
+                episodes.getStyleClass().add("border");
+                season.setAlignment(Pos.CENTER_LEFT);
+                Text seasonText = new Text("Sæson " + seasonCounter + ":");
+                seasonText.styleProperty().set("-fx-font-weight: bold");
+                season.paddingProperty().set(new Insets(8));
 
-                    Button episodeButton = new Button("E" + (ii + 1));
+                seasonText.getStyleClass().add("darkText");
+                season.getChildren().add(seasonText);
+                season.getChildren().add(episodes);
+//                season.setFitToWidth(true);
+//                episodes.setSpacing(8);
+
+                for (int ii = 0; ii < i; ii++) {
+                    Button episodeButton = new Button("" + (ii + 1));
                     episodeButton.styleProperty().set("-fx-margin: 8px");
                     episodeButton.addEventHandler(ActionEvent.ACTION, (ActionEvent e) -> {
                         episodeButton.styleProperty().set("-fx-background-color: blue;");
                     });
-                    season.getChildren().add(episodeButton);
+                    episodes.setMargin(episodeButton, new Insets(8));
+                    episodes.getChildren().add(episodeButton);
                 }
                 left.getChildren().add(season);
+                seasonCounter++;
             }
         }
 
-        left.getChildren().addAll(expandRegion, leftBottomButtons);
-
+//        left.getChildren().addAll(expandRegion);
         RootController.setCurrentRoot(root);
     }
 }
